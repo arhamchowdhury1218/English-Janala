7be9f621af369f6905682051bd869bc6dc53beb5
ärhamchowdhury1218 <arhamchowdhury1218@gmail.com>
2026-02-10 13:55:00 +0600

    completed the hero section and also implemented the form section

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,18 +4,42 @@ const loadLevels = () => {
     .then((data) => displayLevels(data.data));
 };
 
+const loadCourses = (level_no) => {
+  const url = `https://openapi.programming-hero.com/api/level/${level_no}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayCourses(data.data));
+};
+
+const displayCourses = (courses) => {
+  console.log(courses);
+  const coursesContainer = document.getElementById("courses-container");
+
+  coursesContainer.innerHTML = "";
+  courses.forEach((course) => {
+    const courseDiv = document.createElement("div");
+    courseDiv.innerHTML = `
+                  <button class="btn btn-outline btn-secondary">
+                  ${course.word}
+                  </button>
+      `;
+    coursesContainer.appendChild(courseDiv);
+  });
+};
+
 const displayLevels = (levels) => {
   const levelsContainer = document.getElementById("levels-container");
   console.log(levels);
-  //   levelsContainer.innerHTML = "";
-  //   levels.forEach((level) => {
-  //     const levelDiv = document.createElement("div");
-  //     levelDiv.innerHTML = `
-  //       <h2>${level.name}</h2>
-  //       <p>${level.description}</p>
-  //     `;
-  //     levelsContainer.appendChild(levelDiv);
-  //   });
+  levelsContainer.innerHTML = "";
+  levels.forEach((level) => {
+    const levelDiv = document.createElement("div");
+    levelDiv.innerHTML = `
+                  <button onclick = "loadCourses(${level.level_no})" class="btn btn-outline btn-primary">
+                  <i class="fa-solid fa-book-open-reader"></i>Learn - ${level.level_no}
+                  </button>
+      `;
+    levelsContainer.appendChild(levelDiv);
+  });
 };
 
 loadLevels();
