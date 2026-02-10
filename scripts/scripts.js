0ef138f066ad92a6e2f0f426b00cee5c7d487c15
ärhamchowdhury1218 <arhamchowdhury1218@gmail.com>
2026-02-10 15:45:35 +0600

    handling the error

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,6 +16,19 @@ const displayCourses = (courses) => {
   const coursesContainer = document.getElementById("word-container");
 
   coursesContainer.innerHTML = "";
+
+  if (courses.length === 0) {
+    coursesContainer.innerHTML = `
+    <div class="text-center col-span-full font-bangla text-xl py-10 space-y-6 rounded-xl bg-gray-100">
+          <img class="mx-auto" src="./assets/alert-error.png" alt="">      
+          <p class="font-bold text-2xl text-gray-400">
+            এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।
+          </p>
+          <p class="font-extrabold text-4xl">নেক্সট Lesson এ যান</p>
+    </div>`;
+    return;
+  }
+
   courses.forEach((course) => {
     const courseDiv = document.createElement("div");
     courseDiv.innerHTML = `
@@ -23,9 +36,9 @@ const displayCourses = (courses) => {
         <div
           class="bg-white p-6 rounded-lg shadow-md text-center py-10 px-5 space-y-4"
         >
-          <h2 class="text-2xl font-bold mb-4">${course.word}</h2>
+          <h2 class="text-2xl font-bold mb-4">${course.word ? course.word : "No word available"}</h2>
           <p class="font-semibold">Meaning/Pronounciation</p>
-          <div class="text-2xl font-medium font-bangla">"${course.meaning} / ${course.pronunciation}"</div>
+          <div class="text-2xl font-medium font-bangla">"${course.meaning ? course.meaning : "No meaning available"} / ${course.pronunciation ? course.pronunciation : "No pronunciation available"}"</div>
           <div class="flex justify-between">
             <button class="btn btn-outline btn-primary">
               <i class="fa-solid fa-circle-info"></i>
