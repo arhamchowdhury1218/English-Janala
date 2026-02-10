dc65fc206942687b555a85247648826b13723c18
ärhamchowdhury1218 <arhamchowdhury1218@gmail.com>
2026-02-10 13:09:39 +0600

    completed the navbar section

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,21 @@
-console.log("Hello, bhosla");
+const loadLevels = () => {
+  fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then((res) => res.json())
+    .then((data) => displayLevels(data.data));
+};
+
+const displayLevels = (levels) => {
+  const levelsContainer = document.getElementById("levels-container");
+  console.log(levels);
+  //   levelsContainer.innerHTML = "";
+  //   levels.forEach((level) => {
+  //     const levelDiv = document.createElement("div");
+  //     levelDiv.innerHTML = `
+  //       <h2>${level.name}</h2>
+  //       <p>${level.description}</p>
+  //     `;
+  //     levelsContainer.appendChild(levelDiv);
+  //   });
+};
+
+loadLevels();
